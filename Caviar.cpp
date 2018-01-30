10528344d730b3902fa5ba255e9383951c750fd8
mxdmly <mxdmly@qq.com>
2018-01-30 18:54:58 +0800

    QQ

--- Caviar.cpp
+++ Caviar.cpp
@@ -7,19 +7,24 @@
 #include <QFile>
 
 QMutex qlock;
-QDateTime upTime = QDateTime(QDate(2018, 1, 18), QTime(1, 0, 0, 0));
+QDateTime upTime;
 QDateTime thisTime;
 QString cxBAS_str;
-QString crEtonetheS_str;
+QString crEtoneTheS_str;
 QString crEtone_str;
 QString descTOP_str;
-int all_i;
-int get_i;
+int all_i = 0;
+int get_i = 0;
 QString log_str;
 
 Caviar::Caviar()
 {
     ifRun_b = true;
+    upTime = QDateTime(QDate::currentDate().addDays(1), QTime(1, 0, 0, 0));
+    cxBAS_qsq = NULL;
+    crEtone_qsq = NULL;
+    dbBAS_qdb = QSqlDatabase();
+    dbEtone_qdb = QSqlDatabase();
 }
 
 Caviar::~Caviar()
@@ -52,33 +57,39 @@ void Caviar::run()
         qmlock.relock();
 
         thisTime = QDateTime::currentDateTime();//get this time
-        if(1|upTime.operator <=(thisTime))//if thisTime >= upTime
+        if(upTime.operator <=(thisTime))//if thisTime >= upTime
         {
             upTime = upTime.addDays(1);
 //------------------------------------------------------------------------------------------
             iniDB();
             cxBAS_qsq = new QSqlQuery(dbBAS_qdb);
             crEtone_qsq = new QSqlQuery(dbEtone_qdb);
-            log_str = "";
-            log_str = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss ddd");
             qDebug() << QDateTime::currentDateTime();
             qDebug() << "==============================================";
+            log_str = "";
+            log_str.append(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss ddd")).append("\n");
+            log_str.append("==============================================").append("\n");
             qDebug() << "GetData";
+            log_str.append("GetData").append("\n");
             get_i = 0;
-            //getDataTABLEUPLOAD();
+            getDataTABLEUPLOAD();
             qDebug() << "--------HH" << "\t" << all_i << "\t" << get_i;
-            sleep(2);
+            log_str.append("--------HH").append("\t").append(QString::number(all_i, 10)).append("\t").append(QString::number(get_i, 10)).append("\n");
+            sleep(1);
             get_i = 0;
-            //getDataTABLEZ();
+            getDataTABLEZ();
             qDebug() << "--------ZD" << "\t" << all_i << "\t" << get_i;
+            log_str.append("--------ZD").append("\t").append(QString::number(all_i, 10)).append("\t").append(QString::number(get_i, 10)).append("\n");
             get_i = 0;
-            //getDataTABLES();
+            getDataTABLES();
             qDebug() << "--------SS" << "\t" << all_i << "\t" << get_i;
+            log_str.append("--------SS").append("\t").append(QString::number(all_i, 10)).append("\t").append(QString::number(get_i, 10)).append("\n");
             get_i = 0;
-            //getDataTABLEB();
+            getDataTABLEB();
             qDebug() << "------BABY" << "\t" << all_i << "\t" << get_i;
+            log_str.append("------BABY").append("\t").append(QString::number(all_i, 10)).append("\t").append(QString::number(get_i, 10)).append("\n");
 //------------------------------------------------------------------------------------------
-            qDebug() << "+1 Day" << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
+            log_str.append("+1 Day").append(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"));
             writeLog(log_str);
             cxBAS_qsq->clear();
             delete cxBAS_qsq;
@@ -89,8 +100,9 @@ void Caviar::run()
             dbBAS_qdb.close();
             dbEtone_qdb.close();
             qmlock.unlock();
-            //sleep(86000);
+            sleep(86000);
         }
+        qmlock.unlock();
         msleep(2000);
     }
 }
@@ -100,8 +112,8 @@ void Caviar::getDataTABLEUPLOAD()
     crEtone_qsq->exec("SELECT TOP 1 TABLEUPLOAD.FID_TPATIENTVISIT FROM db_upload.dbo.TABLEUPLOAD ORDER BY TABLEUPLOAD.ID DESC");
     if(crEtone_qsq->next())descTOP_str = crEtone_qsq->value(0).toString();
     cxBAS_str = openSqlFile("C:\\Saury\\qTPATIENTVISIT.sql");
-    crEtonetheS_str = openSqlFile("C:\\Saury\\qIntoTABLEUPLOAD_S.sql");
-    cxBAS_str.append("> ").append(descTOP_str).append(" ORDER BY dbo.TPATIENTVISIT.FID ASC");
+    crEtoneTheS_str = openSqlFile("C:\\Saury\\qIntoTABLEUPLOAD_S.sql");
+    cxBAS_str.append("> ").append(descTOP_str).append(" ORDER BY TPATIENTVISIT.FID ASC");
 
     if(!cxBAS_qsq->exec(cxBAS_str))log_str.append(cxBAS_qsq->lastError().text());//to TABLEUPLOAD
     all_i = cxBAS_qsq->numRowsAffected();
@@ -111,7 +123,7 @@ void Caviar::getDataTABLEUPLOAD()
         crEtone_str.append(cxBAS_qsq->value(7).toString()).append("\'");
         crEtone_str.append(" SET @cyt = ").append("\'");
         crEtone_str.append(cxBAS_qsq->value(33).toString()).append("\'").append(" ");
-        crEtone_str.append(crEtonetheS_str);
+        crEtone_str.append(crEtoneTheS_str);
         crEtone_str.append("\'").append(cxBAS_qsq->value(0).toString()).append("\'");
         crEtone_str.append(",@id, @zy, LEFT(@qh, 6),").append("\'").append("01").append("\'");
         crEtone_str.append(",\'").append(cxBAS_qsq->value(1).toString()).append("\'");
@@ -124,10 +136,9 @@ void Caviar::getDataTABLEUPLOAD()
             crEtone_str.append(",\'").append(cxBAS_qsq->value(i).toString()).append("\'");
         }
         crEtone_str.append(",@cbd,@cbx,@cby)");
-        if(crEtone_qsq->exec(crEtone_str))get_i++;
+        if(crEtone_qsq->exec(crEtone_str))get_i += crEtone_qsq->numRowsAffected();
     }
-    writeLog(crEtone_str);
-    if(all_i != get_i)log_str.append("Error" + all_i + get_i);
+    if(all_i != get_i)log_str.append("Error all " + all_i).append(" get " + get_i);
 }
 
 void Caviar::getDataTABLEZ()
@@ -135,26 +146,40 @@ void Caviar::getDataTABLEZ()
     crEtone_qsq->exec("SELECT TOP 1 TABLEZ.ZID FROM db_upload.dbo.TABLEZ ORDER BY TABLEZ.ID DESC");
     if(crEtone_qsq->next())descTOP_str = crEtone_qsq->value(0).toString();
     cxBAS_str = openSqlFile("C:\\Saury\\qZ.sql");
-    crEtonetheS_str = openSqlFile("C:\\Saury\\qIntoZ.sql");
+    crEtoneTheS_str = openSqlFile("C:\\Saury\\qIntoZ.sql");
+    QString crEtoneMin_str = openSqlFile("C:\\Saury\\qIntoZ_not.sql");
     cxBAS_str.append("> ").append(descTOP_str).append(" ORDER BY TDIAGNOSE.FID ASC");
     if(!cxBAS_qsq->exec(cxBAS_str))log_str.append(cxBAS_qsq->lastError().text());//to Z
     all_i = cxBAS_qsq->numRowsAffected();
     while (cxBAS_qsq->next()) {
-        crEtone_str = "DECLARE @bah VARCHAR(20) DECLARE @cs VARCHAR(20) SET @bah = \'";
-        crEtone_str.append(cxBAS_qsq->value(1).toString()).append("\'");
-        crEtone_str.append(" SET @cs = \'").append(cxBAS_qsq->value(2).toString()).append("\'").append(" ");
-        crEtone_str.append(crEtonetheS_str);
-        crEtone_str.append("\'").append(cxBAS_qsq->value(0).toString()).append("\'");
-        crEtone_str.append(",\'").append(cxBAS_qsq->value(1).toString()).append("\'");
-        crEtone_str.append(",\'").append(cxBAS_qsq->value(2).toString()).append("\'");
-        crEtone_str.append(",@zy,@qh,@tp,@sfz");
-        for (int i = 3; i < 9; ++i) {
-            crEtone_str.append(",\'").append(cxBAS_qsq->value(i).toString()).append("\'");
+        if("1" == cxBAS_qsq->value(4).toString() || 1 == cxBAS_qsq->value(4).toString())
+        {
+            crEtone_str = "DECLARE @bah VARCHAR(20) DECLARE @cs VARCHAR(20) SET @bah = \'";
+            crEtone_str.append(cxBAS_qsq->value(1).toString()).append("\'");
+            crEtone_str.append(" SET @cs = \'").append(cxBAS_qsq->value(2).toString()).append("\'").append(" ");
+            crEtone_str.append(crEtoneTheS_str);
+            crEtone_str.append("\'").append(cxBAS_qsq->value(0).toString()).append("\'");
+            crEtone_str.append(",\'").append(cxBAS_qsq->value(1).toString()).append("\'");
+            crEtone_str.append(",\'").append(cxBAS_qsq->value(2).toString()).append("\'");
+            crEtone_str.append(",@zy,@qh,@tp,@sfz");
+            for (int i = 3; i < 9; ++i) {
+                crEtone_str.append(",\'").append(cxBAS_qsq->value(i).toString()).append("\'");
+            }
+            crEtone_str.append(")");
+            if(crEtone_qsq->exec(crEtone_str))get_i += crEtone_qsq->numRowsAffected();
         }
-        crEtone_str.append(")");
-        if(crEtone_qsq->exec(crEtone_str))get_i++;
+        else //减少SQLSERVER冗余，不重复插入非主要诊断的信息
+        {
+            crEtone_str = crEtoneMin_str;
+            crEtone_str.append("\'").append(cxBAS_qsq->value(0).toString()).append("\'");
+            for (int i = 1; i < 9; ++i) {
+                crEtone_str.append(",\'").append(cxBAS_qsq->value(i).toString()).append("\'");
+            }
+            crEtone_str.append(")");
+            if(crEtone_qsq->exec(crEtone_str))get_i += crEtone_qsq->numRowsAffected();
+        }
     }
-    if(all_i != get_i)log_str.append("Error" + all_i + get_i);
+    if(all_i != get_i)log_str.append("Error all " + all_i).append(" get " + get_i);
 }
 
 void Caviar::getDataTABLES()
@@ -162,7 +187,7 @@ void Caviar::getDataTABLES()
     crEtone_qsq->exec("SELECT TOP 1 TABLES.SID FROM db_upload.dbo.TABLES ORDER BY TABLES.ID DESC");
     if(crEtone_qsq->next())descTOP_str = crEtone_qsq->value(0).toString();
     cxBAS_str = openSqlFile("C:\\Saury\\qS.sql");
-    crEtonetheS_str = openSqlFile("C:\\Saury\\qIntoS.sql");
+    crEtoneTheS_str = openSqlFile("C:\\Saury\\qIntoS.sql");
     cxBAS_str.append("> ").append(descTOP_str).append(" ORDER BY TOPERATION.FID ASC");
     if(!cxBAS_qsq->exec(cxBAS_str))log_str.append(cxBAS_qsq->lastError().text());//to S
     all_i =  cxBAS_qsq->numRowsAffected();
@@ -170,7 +195,7 @@ void Caviar::getDataTABLES()
         crEtone_str = "DECLARE @bah VARCHAR(20) DECLARE @cs VARCHAR(20) SET @bah = \'";
         crEtone_str.append(cxBAS_qsq->value(1).toString()).append("\'");
         crEtone_str.append(" SET @cs = \'").append(cxBAS_qsq->value(2).toString()).append("\'").append(" ");
-        crEtone_str.append(crEtonetheS_str);
+        crEtone_str.append(crEtoneTheS_str);
         crEtone_str.append("\'").append(cxBAS_qsq->value(0).toString()).append("\'");
         crEtone_str.append(",\'").append(cxBAS_qsq->value(1).toString()).append("\'");
         crEtone_str.append(",\'").append(cxBAS_qsq->value(2).toString()).append("\'");
@@ -179,9 +204,9 @@ void Caviar::getDataTABLES()
             crEtone_str.append(",\'").append(cxBAS_qsq->value(i).toString()).append("\'");
         }
         crEtone_str.append(")");
-        if(crEtone_qsq->exec(crEtone_str))get_i++;
+        if(crEtone_qsq->exec(crEtone_str))get_i += crEtone_qsq->numRowsAffected();
     }
-    if(all_i != get_i)log_str.append("Error" + all_i + get_i);
+    if(all_i != get_i)log_str.append("Error all " + all_i).append(" get " + get_i);
 }
 
 void Caviar::getDataTABLEB()
@@ -189,15 +214,15 @@ void Caviar::getDataTABLEB()
     crEtone_qsq->exec("SELECT TOP 1 TABLEBABY.BID FROM db_upload.dbo.TABLEBABY ORDER BY TABLEBABY.ID DESC");
     if(crEtone_qsq->next())descTOP_str = crEtone_qsq->value(0).toString();
     cxBAS_str = openSqlFile("C:\\Saury\\qB.sql");
-    crEtonetheS_str = openSqlFile("C:\\Saury\\qIntoB.sql");
-    cxBAS_str.append("> ").append(descTOP_str).append(" ORDER BY dbo.TBABYCARD.FID ASC");
+    crEtoneTheS_str = openSqlFile("C:\\Saury\\qIntoB.sql");
+    cxBAS_str.append("> ").append(descTOP_str).append(" ORDER BY TBABYCARD.FID ASC");
     if(!cxBAS_qsq->exec(cxBAS_str))log_str.append(cxBAS_qsq->lastError().text());//to B
     all_i =  cxBAS_qsq->numRowsAffected();
     while (cxBAS_qsq->next()) {
         crEtone_str = "DECLARE @bah VARCHAR(20) DECLARE @cs VARCHAR(20) SET @bah = \'";
         crEtone_str.append(cxBAS_qsq->value(1).toString()).append("\'");
         crEtone_str.append(" SET @cs = \'").append(cxBAS_qsq->value(2).toString()).append("\'").append(" ");
-        crEtone_str.append(crEtonetheS_str);
+        crEtone_str.append(crEtoneTheS_str);
         crEtone_str.append("\'").append(cxBAS_qsq->value(0).toString()).append("\'");
         crEtone_str.append(",\'").append(cxBAS_qsq->value(1).toString()).append("\'");
         crEtone_str.append(",\'").append(cxBAS_qsq->value(2).toString()).append("\'");
@@ -206,9 +231,9 @@ void Caviar::getDataTABLEB()
             crEtone_str.append(",\'").append(cxBAS_qsq->value(i).toString()).append("\'");
         }
         crEtone_str.append(")");
-        if(crEtone_qsq->exec(crEtone_str))get_i++;
+        if(crEtone_qsq->exec(crEtone_str))get_i += crEtone_qsq->numRowsAffected();
     }
-    if(all_i != get_i)log_str.append("Error" + all_i + get_i);
+    if(all_i != get_i)log_str.append("Error all " + all_i).append(" get " + get_i);
 }
 
 void Caviar::iniDB()
@@ -217,15 +242,15 @@ void Caviar::iniDB()
     {
         dbBAS_qdb = QSqlDatabase::addDatabase("QODBC", "dbBAS");
         dbBAS_qdb.setDatabaseName("dbBAS");//数据连接名，在控制面板设置
-        dbBAS_qdb.setUserName("william");
-        dbBAS_qdb.setPassword("");
+        dbBAS_qdb.setUserName("YBB");
+        dbBAS_qdb.setPassword("0");
     }
     if(!dbEtone_qdb.isValid())
     {
-        dbEtone_qdb = QSqlDatabase::addDatabase("QODBC", "dbEtone");
-        dbEtone_qdb.setDatabaseName("dbEtone");//数据连接名，在控制面板设置
-        dbEtone_qdb.setUserName("sa");
-        dbEtone_qdb.setPassword("sa123!@#");
+        dbEtone_qdb = QSqlDatabase::addDatabase("QODBC", "dbCaviar");
+        dbEtone_qdb.setDatabaseName("dbCaviar");//数据连接名，在控制面板设置
+        dbEtone_qdb.setUserName("YBB");
+        dbEtone_qdb.setPassword("0");
     }
 
     if(!dbBAS_qdb.open())
@@ -266,5 +291,6 @@ void Caviar::writeLog(QString str)
     QFile f("C:\\Saury\\out.log");
     f.open(QIODevice::Text | QIODevice::Append);
     QTextStream textStream(&f);
-    textStream << str << endl << endl;
+    textStream << str << endl;
+    f.close();
 }
